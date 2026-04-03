78805965efbb687d8e6943584262853d534c6446
Scarlet Sypher <ayushjha21157@gmail.com>
2026-04-03 16:34:32 +0530

    modified: simulation api call

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
+import type { SimulationInput, SimulationResult } from "../types/simulation";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+export const runSimulation = async (input: SimulationInput): Promise<SimulationResult> => {
+
+
+  const res = await api.post("/api/simulate", input);
+  return res.data;
+
+  
+};
